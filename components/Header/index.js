6068c78e6c3834f198caa204d80b0335c6602831
lauralyeinc <52268790+lauralyeinc@users.noreply.github.com>
2019-09-15 21:54:06 -0400

    had a syntax error with append child, no quotes around the child to the parent ever.

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,25 +11,24 @@
 
 function Header() {
   //create elements 
-  const headerDiv = document.createElement("div");
-  const date = document.createElement("span");
-  const title = document.createElement("h1");
-  const temp = document.createElement("span");
-  
-  //content 
-  date.textContent = "SMARCH 28, 2019";
-  title.textContent = "Lambda Times";
-  temp.textContent = "98 degrees"; 
+  let headerDiv = document.createElement("div");
+  let dateHeader = document.createElement("span");
+  let titleHeader = document.createElement("h1");
+  let tempHeader = document.createElement("span");
 
   //structure 
-  headerDiv.appendChild("date");
-  date.appendChild("title");
-  date.appendChild("temp");
-
-  //styles 
+  headerDiv.appendChild(dateHeader);
+  headerDiv.appendChild(titleHeader);
+  headerDiv.appendChild(tempHeader);
+  
+  //content 
+  dateHeader.textContent = "SMARCH 28, 2019";
+  titleHeader.textContent = "Lambda Times";
+  tempHeader.textContent = "98 degrees"; 
+  //style   
   headerDiv.classList.add("header");
-  date.classList.add("date");
-  temp.classList.add("temp");
+  dateHeader.classList.add("date");
+  tempHeader.classList.add("temp");
 
   return headerDiv; 
 }
